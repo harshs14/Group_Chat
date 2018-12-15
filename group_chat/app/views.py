23573d48be93f4943ad66426756f228f9ee40f10
harshs14 <harsh.singh1411@gmail.com>
2018-12-15 19:07:37 +0530

    user registeration done

--- group_chat/app/views.py
+++ group_chat/app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
+from app.serializers import UserSerializer
 
-# Create your views here.
+
+# -----------------------User Registeration/Signup----------------------------------------------------------------------
+class Register(generics.ListCreateApiView):
+
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+
+        user = User.objects.create_user(name=request.POST.get('name'),
+                                        email=request.POST.get('email'),
+                                        username=request.POST.get('username'),
+                                        password=request.POST.get('password'))
+
+        user.save(commit=False)
+        user.is_active = False
+        user.save()
+
+
