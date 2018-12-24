bcaeb0bae3db8494400f605dbbb79a1c657d2216
harshs14 <harsh.singh1411@gmail.com>
2018-12-24 15:39:37 +0530

    singup progress

--- group_chat/app/serializers.py
+++ group_chat/app/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        required=True,
+        min_length=8)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'],
+                                        validated_data['password'])
+        return user
 
     class Meta:
         model = User
-        fields = ('name', 'email', 'username', 'password')
+        fields = ('id', 'first_name', 'last_name', 'email', 'username', 'password',)
--- group_chat/app/views.py
+++ group_chat/app/views.py
@@ -1,22 +1,27 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, status
 from django.contrib.auth.models import User
 from app.serializers import UserSerializer
+from rest_framework.response import Response
 
 
-# -----------------------User Registeration/Signup----------------------------------------------------------------------
-class Register(generics.ListCreateApiView):
-
+# -----------------------User Registration/Signup-------------------------------------------------------------------
+class Register(generics.ListCreateAPIView):
+    queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (permissions.AllowAny,)
 
     def post(self, request, *args, **kwargs):
 
-        user = User.objects.create_user(name=request.POST.get('name'),
+        user = User.objects.create_user(first_name=request.POST.get('first_name'),
+                                        last_name=request.POST.get('last_name'),
                                         email=request.POST.get('email'),
                                         username=request.POST.get('username'),
-                                        password=request.POST.get('password'))
+                                        password1=request.POST.get('password'))
 
         user.save(commit=False)
         user.is_active = False
         user.save()
+
+        return Response({'info': 'user created'}, status=status.HTTP_201_CREATED)
 
 
