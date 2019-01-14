274cebc5d3d01e8a08f41866ffbf58d6a4f5fbc9
harshs14 <harsh.singh1411@gmail.com>
2019-01-14 18:03:19 +0530

    permission for ismessageowner added

--- group_chat/app/permissions.py
+++ group_chat/app/permissions.py
@@ -19,3 +19,21 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.username == request.user.username
+
+
+class IsGroupMember(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        return obj.members == request.user.username
+
+
+class IsMessageOwner(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.messaged_by == request.user
+
