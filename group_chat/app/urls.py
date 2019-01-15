36a86f36eade6418975d8b3eb08b8d2ca7db8136
harshs14 <harsh.singh1411@gmail.com>
2019-01-15 23:49:43 +0530

    user's group list

--- group_chat/app/urls.py
+++ group_chat/app/urls.py
@@ -14,13 +14,14 @@ urlpatterns = [
     re_path(r'^login/$', views.Login.as_view(), name='login'),
     re_path(r'^profile/(?P<id>[0-9]+)/$', views.UserProfile.as_view(), name='user_profile'),
     # re_path(r'^createprofile/$', views.CreateUserProfile.as_view(), name='create_profile'),
-    re_path(r'^group/$', views.Groups.as_view(), name='create_group'),
+    re_path(r'^creategroup/$', views.CreateGroups.as_view(), name='create_group'),
     re_path(r'^groupprofile/(?P<id>[0-9]+)/$', views.GroupProfile.as_view(), name='group_profile'),
     re_path(r'^logout/$', views.Logout.as_view(), name='logout'),
     # re_path(r'^addmembers/(?P<id>[0-9]+)/', views.AddMember.as_view(), name='add_member'),
     # re_path(r'^contactlist/$', views.ContactList.as_view(), name='contact_list'),
     re_path(r'^group/(?P<g_id>[0-9]+)/$', views.Message.as_view(), name='message'),
     re_path(r'^group/(?P<g_id>[0-9]+)/(?P<id>[0-9]+)/$', views.Message.as_view(), name='message_detail'),
+    re_path(r'^group/$', views.GroupView.as_view(), name='group'),
     re_path(r'^docs/', include_docs_urls(title='My API title'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
