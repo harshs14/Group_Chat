46f0e3e987ecd7f819fda7f39587895e1c1ca375
harshs14 <harsh.singh1411@gmail.com>
2019-02-02 14:55:49 +0530

    setting up basic consumer and socket

--- group_chat/group_chat/routing.py
+++ group_chat/group_chat/routing.py
@@ -2,12 +2,17 @@ from django.conf.urls import url
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from app.consumers import GroupMessageConsumer
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+
 
 application = ProtocolTypeRouter({
-    "websocket": AuthMiddlewareStack(
-        URLRouter([
-           url(r'^test_group/(?P<g_id>[0-9]+)/$', GroupMessageConsumer),
-        ])
-    ),
-
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+           URLRouter(
+               [
+                   url(r'^test_group/(?P<g_id>[0-9]+)/$', GroupMessageConsumer),
+               ]
+           )
+        )
+    )
 })
