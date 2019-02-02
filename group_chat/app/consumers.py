441593272969adea316dc7d1c02a4f7268b60695
harshs14 <harsh.singh1411@gmail.com>
2019-02-02 14:55:49 +0530

    setting up basic consumer and socket

--- group_chat/app/consumers.py
+++ group_chat/app/consumers.py
@@ -1,21 +1,38 @@
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.consumer import AsyncConsumer
+from .models import *
+import asyncio
+from channels.db import database_sync_to_async
 
 
-class GroupMessageConsumer(AsyncJsonWebsocketConsumer):
+class GroupMessageConsumer(AsyncConsumer):
 
-    async def connect(self):
+    async def websocket_connect(self, event):
+        print("connnected", event)
 
-        if self.scope["user"].is_anonymous:
-            print("anonymous")
-            await self.close()
-        else:
-            print("accepted")
-            await self.accept()
+    async def websocket_recieve(self, event):
+        print("connnected", event)
 
-    async def receive(self, text_data=None, bytes_data=None, **kwargs):
-        print("receiving")
-        await self.send(text_data="receiving")
+    async def websocket_disconnect(self, event):
+        print("connnected", event)
 
-    async def close(self, code=None):
-        print("closed")
-        await self.close()
+
+#
+# class GroupMessageConsumer(AsyncWebsocketConsumer):
+#
+#     async def connect(self):
+#
+#         if self.scope["user"].is_anonymous:
+#             print("anonymous")
+#             await self.close()
+#         else:
+#             print("accepted")
+#             await self.accept()
+#
+#     async def receive(self, text_data=None, bytes_data=None, **kwargs):
+#         print("receiving")
+#         await self.send(text_data="receiving")
+#
+#     async def close(self, code=None):
+#         print("closed")
+#         await self.close()
