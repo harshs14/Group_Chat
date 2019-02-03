21191b62bc4e0cc1d8de7cb527361aac4b822c46
harshs14 <harsh.singh1411@gmail.com>
2019-02-03 19:06:07 +0530

    messages are live

--- group_chat/app/consumers.py
+++ group_chat/app/consumers.py
@@ -9,6 +9,12 @@ class GroupMessageConsumer(AsyncConsumer):
 
     async def websocket_connect(self, event):
         print("connected", event)
+        self.group = self.scope['url_route']['kwargs']['g_id']
+
+        await self.channel_layer.group_add(
+            self.group,
+            self.channel_name
+        )
         await self.send({
             "type": "websocket.accept",
         })
@@ -20,11 +26,23 @@ class GroupMessageConsumer(AsyncConsumer):
             loaded_dict_data = json.loads(front_text)
             msg = loaded_dict_data.get('message')
             print(msg)
-            await self.send({
-                "type": "websocket.send",
-                "text": msg
-            })
+            # await self.send({
+            #     "type": "websocket.send",
+            #     "text": msg
+            # })
+            await self.channel_layer.group_send(
+                self.group,
+                {
+                    'type': 'chat_message',
+                    'text': msg
+                }
+            )
 
     async def websocket_disconnect(self, event):
         print("disconnected", event)
 
+    async def chat_message(self, event):
+        await self.send({
+            "type": "websocket.send",
+            "text": event['text']
+        })
